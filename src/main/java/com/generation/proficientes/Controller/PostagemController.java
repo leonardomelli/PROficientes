fd04f6e953267d2bfad06be5b641cf63be917b00
Rosendo Neto <rosendo.fontes@mercadolivre.com>
2023-02-03 10:35:01 -0300

    Add files via upload

---
+++ src/main/java/com/generation/proficientes/Controller/PostagemController.java
@@ -0,0 +1,13 @@
+package com.generation.proficientes.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagem")
+@CrossOrigin
+public class PostagemController {
+
+
+}
