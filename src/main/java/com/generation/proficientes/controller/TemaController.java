61dc398980280f5b8375f135938d2f48c896666d
Edviges de Lima <edviges.lima@mercadolivre.com>
2023-02-14 11:18:59 -0300

    CRUD de classe Tema

--- src/main/java/com/generation/proficientes/controller/TemaController.java
+++ src/main/java/com/generation/proficientes/controller/TemaController.java
@@ -1,12 +1,15 @@
 package com.generation.proficientes.controller;
 import com.generation.proficientes.repository.TemaRepository;
 import com.generation.proficientes.model.Tema;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/temas")
@@ -28,5 +31,33 @@ public class TemaController {
                 .map(resposta -> ResponseEntity.ok(resposta))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 
+    }
+    @PostMapping
+    public ResponseEntity<Tema> post(@Valid @RequestBody Tema tema){
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(temaRepository.save(tema));
+
+    }
+
+    @PutMapping
+    public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema){
+
+        return temaRepository.findById(tema.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
+                        .body(temaRepository.save(tema)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
+        Optional<Tema> tema = temaRepository.findById(id);
+
+        if (tema.isEmpty())
+            throw  new ResponseStatusException(HttpStatus.NOT_FOUND);
+        temaRepository.deleteById(id);
     }
 }
